33c0a1bb29952df8f7011611f3da67cf2cc4e43f
szabolcsit <szabolcsit@hu.ibm.com>
2018-11-29 19:20:49 +0100

    Update index.js

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ if(process.env.NODE_ENV !== "production") {
 } else {
 	client = redis.createClient(process.env.REDIS_URL)
 }
+const {promisify} = require('util')
 const getAsync = promisify(client.get).bind(client);
 const restify = require('restify');
 const crypto = require('crypto');
