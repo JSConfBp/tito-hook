89352feb3f652153f3dae2f33014c33186a8e4d4
Szabolcs Szabolcsi-Tóth <szabolcsit@hu.ibm.com>
2020-03-04 20:21:54 +0100

    cleanup

--- index.js
+++ index.js
@@ -1,57 +1,47 @@
-if(process.env.NODE_ENV !== "production") {
+if(process.env.NODE_ENV !== 'production') {
 	require('dotenv').config()
 }
 
-const TITO_JS_TOKEN = process.env.TITO_JS_TOKEN
-const TITO_CSS_TOKEN = process.env.TITO_CSS_TOKEN
-const redis = require("redis")
+const { promisify } = require('util')
+const restify = require('restify');
+const crypto = require('crypto');
+const redis = require('redis')
+
+const { TITO_SECURITY_TOKEN } = process.env
+
 let client
 
-if(process.env.NODE_ENV !== "production") {
+if(process.env.NODE_ENV !== 'production') {
 	client = redis.createClient({
     	host: process.env.REDIS_URL
 	})
 } else {
 	client = redis.createClient(process.env.REDIS_URL)
 }
-const { promisify } = require('util')
+
 const setAsync = promisify(client.set).bind(client)
 const getAsync = promisify(client.get).bind(client)
 const delAsync = promisify(client.del).bind(client)
-const restify = require('restify');
-const crypto = require('crypto');
 
 async function webhook(req, res, next) {
 	res.send('ok');
 
 	const signature = req.headers['tito-signature']
 
-	const hmacJS = crypto.createHmac('sha256', TITO_JS_TOKEN);
+	const hmacJS = crypto.createHmac('sha256', TITO_SECURITY_TOKEN);
 	hmacJS.update(JSON.stringify(req.body));
 
-	const hmacCSS = crypto.createHmac('sha256', TITO_CSS_TOKEN);
-	hmacCSS.update(JSON.stringify(req.body));
-
 	const pass = (signature === hmacJS.digest('base64'))
-		|| (signature === hmacCSS.digest('base64'));
-
-
-	console.log({
-		pass,
-		fromJS: signature === hmacJS.digest('base64'),
-		fromCSS: signature === hmacCSS.digest('base64')
-	})
-
 
 	if (pass) {
-		const { state_name, reference, updated_at = ''} = req.body
+		const { state_name, reference, updated_at = '' } = req.body
 
 		if (state_name === 'new') {
 			await setAsync(reference, updated_at)
-			console.log(reference, updated_at)
+			console.log('set', reference, updated_at)
 		}
 
-		if (state_name === "void") {
+		if (state_name === 'void') {
 			await delAsync(reference)
 			console.log('removed', reference)
 		}
@@ -62,6 +52,7 @@ async function webhook(req, res, next) {
 
 async function check (req, res, next) {
 	const id = req.params.titoid
+
 	const result = await getAsync(id)
 
 	if (result) {
---
+++ scripts/encode-payload.js
@@ -0,0 +1,12 @@
+require('dotenv').config()
+
+const crypto = require('crypto');
+const { TITO_SECURITY_TOKEN } = process.env
+
+const hmacJS = crypto.createHmac('sha256', TITO_SECURITY_TOKEN);
+
+console.log("Encoding ", process.argv[2])
+
+hmacJS.update(process.argv[2]);
+
+console.log('tito-signature:' , hmacJS.digest('base64'))
