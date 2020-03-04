223d010baad44d6ef7eb9f14e4c0c45fc5777239
Szabolcs Szabolcsi-Tóth <szabolcsit@hu.ibm.com>
2020-03-04 21:17:15 +0100

    cleanup, lint

--- README.md
+++ README.md
@@ -1,3 +1,82 @@
 # tito-hook
 
-[![Greenkeeper badge](https://badges.greenkeeper.io/JSConfBp/tito-hook.svg)](https://greenkeeper.io/)
+Small service that registers Ti.to ticket references via webhooks.
+
+We use this service to check registrations for our workshops, without moving any personal information. We deploy this to Heroku.
+
+## API
+
+#### Check if a registration is existing
+
+```
+curl --location --request GET 'http://0.0.0.0:8080/check/AAAA-1'
+```
+
+Returns 200 if the registration exists or 404 otherwise.
+
+#### Register a new ticket purchase
+
+```
+curl --location --request POST 'http://0.0.0.0:8080/webhook' \
+--header 'tito-signature: 6wuxCpmNlLFTQyTc6zdnTIYCPe04YusZ8+y0UQl5cFU=' \
+--header 'Accept: application/json' \
+--header 'Content-Type: application/json' \
+--data-raw '{
+    "state_name": "new",
+    "reference": "ABCD-1",
+    "updated_at": "now"
+}'
+````
+
+This is based on how the [ti.to webhooks](https://ti.to/docs/api/admin#webhooks-payloads) work.
+
+## Development
+
+- Clone/fork the repo.
+- Rename `.env.example` to `.env` and add your `TITO_SECURITY_TOKEN` _(it's displayed in the tito admin, Customize / Webhooks menu)_
+- `npm install`
+- Start the dev environment using `npm run dev`
+
+This will spin up a few docker containers:
+
+- node 12
+- redis
+- redis commander _(useful to debug redis content)_
+
+You can create a test signature for a payload using `scripts/encode-payload.js`
+
+
+```bash
+$ node scripts/encode-payload.js '{"state_name":"new","reference":"ABCD-1","updated_at":"now"}'
+
+Encoding  {"state_name":"new","reference":"ABCD-1","updated_at":"now"}
+tito-signature: 6wuxCpmNlLFTQyTc6zdnTIYCPe04YusZ8+y0UQl5cFU=
+```
+
+Use this signature in the `tito-signature` headers in your POST request if you want to test locally.
+
+
+## License
+
+MIT License
+
+Copyright (c) 2018 JSConf Budapest
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
--- index.js
+++ index.js
@@ -1,81 +1,81 @@
-if(process.env.NODE_ENV !== 'production') {
-	require('dotenv').config()
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
 }
 
 const { promisify } = require('util')
-const restify = require('restify');
-const crypto = require('crypto');
+const restify = require('restify')
+const crypto = require('crypto')
 const redis = require('redis')
 
 const { TITO_SECURITY_TOKEN } = process.env
 
 let client
 
-if(process.env.NODE_ENV !== 'production') {
-	client = redis.createClient({
-    	host: process.env.REDIS_URL
-	})
+if (process.env.NODE_ENV !== 'production') {
+  client = redis.createClient({
+    host: process.env.REDIS_URL
+  })
 } else {
-	client = redis.createClient(process.env.REDIS_URL)
+  client = redis.createClient(process.env.REDIS_URL)
 }
 
 const setAsync = promisify(client.set).bind(client)
 const getAsync = promisify(client.get).bind(client)
 const delAsync = promisify(client.del).bind(client)
 
-async function webhook(req, res, next) {
-	res.send('ok');
+async function webhook (req, res, next) {
+  res.send('ok')
 
-	const signature = req.headers['tito-signature']
+  const signature = req.headers['tito-signature']
 
-	const hmacJS = crypto.createHmac('sha256', TITO_SECURITY_TOKEN);
-	hmacJS.update(JSON.stringify(req.body));
+  const hmacJS = crypto.createHmac('sha256', TITO_SECURITY_TOKEN)
+  hmacJS.update(JSON.stringify(req.body))
 
-	const pass = (signature === hmacJS.digest('base64'))
+  const pass = (signature === hmacJS.digest('base64'))
 
-	if (pass) {
-		const { state_name, reference, updated_at = '' } = req.body
+  if (pass) {
+    const { state_name: stateName, reference, updated_at: updatedAt = '' } = req.body
 
-		if (state_name === 'new') {
-			await setAsync(reference, updated_at)
-			console.log('set', reference, updated_at)
-		}
+    if (stateName === 'new') {
+      await setAsync(reference, updatedAt)
+      console.log('set', reference, updatedAt)
+    }
 
-		if (state_name === 'void') {
-			await delAsync(reference)
-			console.log('removed', reference)
-		}
-	}
+    if (stateName === 'void') {
+      await delAsync(reference)
+      console.log('removed', reference)
+    }
+  }
 
-  	next();
+  next()
 }
 
 async function check (req, res, next) {
-	const id = req.params.titoid
+  const id = req.params.titoid
 
-	const result = await getAsync(id)
+  const result = await getAsync(id)
 
-	if (result) {
-		res.status(200);
-		res.send('200')
-	} else {
-		res.status(404);
-		res.send('404')
-	}
+  if (result) {
+    res.status(200)
+    res.send('200')
+  } else {
+    res.status(404)
+    res.send('404')
+  }
 
-	next()
+  next()
 }
 
-const server = restify.createServer();
+const server = restify.createServer()
 
-server.post('/webhook', webhook);
-server.head('/webhook', webhook);
+server.post('/webhook', webhook)
+server.head('/webhook', webhook)
 
-server.get('/check/:titoid', check);
-server.head('/check/:titoid', check);
+server.get('/check/:titoid', check)
+server.head('/check/:titoid', check)
 
 server.use(restify.plugins.bodyParser())
 
-server.listen(process.env.PORT, function() {
-  console.log('%s listening at %s', server.name, server.url);
-});
+server.listen(process.env.PORT, function () {
+  console.log('%s listening at %s', server.name, server.url)
+})
